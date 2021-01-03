7a4e6a8736307c1bdd7fda2c6598c2618faea980
Tomoyuki-Yoshida-1983 <address.t.y@icloud.com>
2021-01-03 13:31:03 +0900

    写真のメタデータをDBに登録・取得し、画面表示できるようにした

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -6,8 +6,10 @@ CREATE TABLE IF NOT EXISTS photos (
     extension VARCHAR NOT NULL,
     width VARCHAR NULL,
     height VARCHAR NULL,
-    shooting_date_time VARCHAR NULL,
+    shooting_date_time TIMESTAMP NULL,
     latitude VARCHAR NULL,
+    latitude_ref VARCHAR NULL,
     longitude VARCHAR NULL,
+    longitude_ref VARCHAR NULL,
     PRIMARY KEY (id)
 );
