d243759de2d59c0bc0cd0c24953943a5ae7d8cbd
Tomoyuki-Yoshida-1983 <76816531+Tomoyuki-Yoshida-1983@users.noreply.github.com>
2021-01-14 19:49:47 +0900

    エラー画面の文言を多言語化 JSのメッセージの取得方法を改善

--- src/main/resources/static/js/photo.js
+++ src/main/resources/static/js/photo.js
@@ -1,10 +1,8 @@
 var PhotoJs = {
 
-    JS_CONFIRM_DELETE_THIS_PHOTO: document.getElementById("JS_CONFIRM_DELETE_THIS_PHOTO").value,
-
     preDeleteCheck: function() {
 
-        if (confirm(this.JS_CONFIRM_DELETE_THIS_PHOTO)) {
+        if (confirm(JS_CONFIRM_DELETE_THIS_PHOTO)) {
             return true;
         }
 
